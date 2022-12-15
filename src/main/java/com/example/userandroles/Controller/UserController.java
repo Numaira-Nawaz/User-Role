474ef4a774e404a080fd4cb7201a169b15a7fff5
Numaira-Nawaz <n.numaira1129@gmail.com>
2022-12-15 12:48:24 +0500

    remove

--- src/main/java/com/example/userandroles/Controller/UserController.java
+++ src/main/java/com/example/userandroles/Controller/UserController.java
@@ -12,11 +12,8 @@ public class UserController {
 
     @PostMapping("/adduser")
     public User addUser(@RequestBody User user){
-
         System.out.println("hello");
-
         System.out.println("The user is added");
-
         return userService.addUser1(user);
     }
     //UPDATE-USER
